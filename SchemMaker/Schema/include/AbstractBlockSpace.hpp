d2a24a158a626ceb286499d192b5180e2cd122cf
A_Lingll <Trinullais000@gmail.com>
2024-03-16 20:35:56 +0800

    aa

--- SchemMaker/Schema/include/AbstractBlockSpace.hpp
+++ SchemMaker/Schema/include/AbstractBlockSpace.hpp
@@ -5,21 +5,30 @@
 namespace Schema {
 	template<typename T>
 	class AbstractBlockSpace{
-
+	public:
+		struct Shape {
+			unsigned short width;
+			unsigned short height;
+			unsigned short lenth;
+		};
+		struct Pos {
+			unsigned short x;
+			unsigned short y;
+			unsigned short z;
+		};
+		
 	private:
-		std::vector<T> _block_space;
-		unsigned short _width;
-		unsigned short _height;
-		unsigned short _lenth;
+		std::vector<T> _data;
+		Shape _shape;
 
 	public:
 		AbstractBlockSpace(const std::vector<T>& block_space, unsigned short width, unsigned short height, unsigned short lenth) :
-			_block_space(block_space), _width(width), _height(height), _lenth(lenth) {}
+			_data(block_space), _shape({width,height,lenth}) {}
 
 		AbstractBlockSpace(std::vector<T>&& block_space, unsigned short width, unsigned short height, unsigned short lenth) :
-			_block_space(std::move(block_space)), _width(width), _height(height), _lenth(lenth) {}
+			_data(std::move(block_space)), _width(width), _height(height), _lenth(lenth) {}
 
-		bool is_legal() { return _width * _height * _lenth == _block_space.size(); }
+		bool is_legal() { return _shape.width * _shape.height * _shape.lenth == _data.size(); }
 	};
 }
 
